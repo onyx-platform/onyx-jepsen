4dffd51722659b652fd793751e81d9616db24c6b
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-12-14 21:18:35 +0800

    Docker machine creation script - vmware fusion only

--- script/create-machine.sh
+++ script/create-machine.sh
@@ -1,2 +1,2 @@
-docker-machine create --driver vmwarefusion --vmwarefusion-disk-size 50000 --vmwarefusion-memory-size 20000 --vmwarefusion-cpu-count "5" jepsen-vmware
+docker-machine create --driver vmwarefusion --vmwarefusion-disk-size 50000 --vmwarefusion-memory-size 20000 --vmwarefusion-cpu-count "6" jepsen-vmware
 eval "$(docker-machine env jepsen-vmware)"
