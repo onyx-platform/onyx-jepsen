4207560a829a1fefcae5182841dae87c46a40246
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-12-14 21:15:05 +0800

    More advanced run-peers.sh script.

--- script/run-peers.sh
+++ script/run-peers.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+### Script for Jepsen to run inside nodes to startup peers
+
 mount -t tmpfs -o remount,rw,nosuid,nodev,noexec,relatime,size=1024M tmpfs /dev/shm
 
-nohup java -cp onyx-peers.jar onyx_peers.launcher.launch_prod_peers 4 > peers-out.log 2>&1 < /dev/null &
+BIND_ADDR=$(ifconfig eth0 | grep "inet addr:" | cut -d : -f 2 | cut -d " " -f 1)
+N_PEERS=$1
+
+nohup java -D"aeron.client.liveness.timeout=50000000000" -D"aeron.threading.mode=SHARED" -server -XX:+UseG1GC \
+                        -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -XX:+UnlockDiagnosticVMOptions \
+			-XX:StartFlightRecording="duration=1080s,filename=myrecording.jfr" \
+	-cp onyx-peers.jar onyx_peers.launcher.launch_prod_peers $N_PEERS $BIND_ADDR > peers-out.log 2>&1 < /dev/null &
 
 if [ $? -eq 0 ]
 then
