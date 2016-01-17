fa40b9697a9e728773ecee8ac688b940dfa21ca9
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-01-17 16:44:10 +0800

    Better docs, fix versions

--- README.md
+++ README.md
@@ -74,7 +74,7 @@ onyx-bookkeeper plugin:
 
 ## License
 
-Copyright © 2015 FIXME
+Copyright © 2015 Distributed Masonry LLC
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :jvm-opts ^:replace ["-server" "-Xmx6g"]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.onyxplatform/onyx "0.8.5-SNAPSHOT" :exclusions [org.slf4j/slf4j-nop]]
-                 [org.onyxplatform/onyx-bookkeeper "0.8.5.1-SNAPSHOT"]
+                 [org.onyxplatform/onyx-bookkeeper "0.8.4.1-SNAPSHOT"]
                  [jepsen "0.0.6"]])
