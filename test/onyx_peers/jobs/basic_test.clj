dd438075d2d6fc6c3d3c45bc4faee27a6b970f06
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-01-18 20:11:43 +0800

    Add test-setup map to the unit test

--- test/onyx_peers/jobs/basic_test.clj
+++ test/onyx_peers/jobs/basic_test.clj
@@ -22,20 +22,36 @@
         peer-config (assoc (:peer-config config) :onyx/id id)
         test "basic-clojure.test"
         version "dummy-version"
-        {:keys [client checker model generator] :as basic-test} (oj/basic-test env-config peer-config version)
-        batch-size 2
+        test-setup {:job-params {:batch-size 1}
+                    :nemesis :na
+                    ; unused in this test
+                    :time-limit 800
+                    :awake-mean 200
+                    :stopped-mean 100
+                    ; may or may not work when 5 is not divisible by n-jobs
+                    :n-jobs 1
+                    ; Minimum total = 5 (input ledgers) + 1 intermediate + 1 output
+                    :n-peers 3}
+        fake-clients 5
+        n-peers-total (* fake-clients (:n-peers test-setup))
+        {:keys [client checker model generator] :as basic-test} (oj/basic-test env-config peer-config test-setup version)
         events [{:type :invoke :f :add :value 1}
                 {:type :invoke :f :add :value 2}
                 {:type :invoke :f :add :value 3}
                 {:type :invoke :f :add :value 4}
                 {:type :invoke :f :add :value 5}
                 {:type :invoke :f :add :value 6}
-                {:type :invoke :f :submit-job :job-num 0 :n-jobs 1 :params {:batch-size 1}}
+                {:type :invoke 
+                 :f :submit-job 
+                 :job-type :simple-job 
+                 :job-num 0 
+                 :n-jobs (:n-jobs test-setup) 
+                 :params (:job-params test-setup)}
                 {:type :invoke :f :close-ledgers-await-completion}
                 {:type :invoke :f :read-ledgers}
                 {:type :invoke :f :read-peer-log :timeout 1000}]
         simple-gen (gen/seq events)]
-    (with-test-env [test-env [15 env-config peer-config]]
+    (with-test-env [test-env [n-peers-total env-config peer-config]]
       (let [setup-client (client/setup! client "onyx-unit" "n1")
             history (reduce (fn [vs event]
                               (conj vs event (client/invoke! setup-client test (gen/op simple-gen test 0))))
