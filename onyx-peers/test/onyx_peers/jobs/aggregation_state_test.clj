0ea11d620d800b6dde7db99aab1ac2c5bd2e0fc8
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-01-18 20:15:15 +0800

    Re-add aggregation-state-test in prep for jepsenisation

---
+++ onyx-peers/test/onyx_peers/jobs/aggregation_state_test.clj
@@ -0,0 +1,169 @@
+(ns onyx-peers.jobs.aggregation-state-test
+  (:require [clojure.core.async :refer [chan >!! <!! close! sliding-buffer]]
+            [clojure.test :refer [deftest is]]
+            [onyx.plugin.core-async :refer [take-segments!]]
+            [onyx.test-helper :refer [load-config with-test-env]]
+            [taoensso.timbre :refer [fatal info]]
+            [onyx.api]))
+
+(def input
+  [{:id 1  :age 21 :event-time #inst "2015-09-13T03:00:00.829-00:00"}
+   {:id 2  :age 12 :event-time #inst "2015-09-13T03:04:00.829-00:00"}
+   {:id 3  :age 3  :event-time #inst "2015-09-13T03:05:00.829-00:00"}
+   {:id 3  :age 3  :event-time #inst "2015-09-13T03:05:00.829-00:00"}
+   {:id 3  :age 3  :event-time #inst "2015-09-13T03:05:00.829-00:00"}
+   {:id 4  :age 64 :event-time #inst "2015-09-13T03:06:00.829-00:00"}
+   {:id 5  :age 53 :event-time #inst "2015-09-13T03:07:00.829-00:00"}
+   {:id 6  :age 52 :event-time #inst "2015-09-13T03:08:00.829-00:00"}
+   {:id 7  :age 24 :event-time #inst "2015-09-13T03:09:00.829-00:00"}
+   {:id 8  :age 35 :event-time #inst "2015-09-13T03:15:00.829-00:00"}
+   {:id 9  :age 49 :event-time #inst "2015-09-13T03:25:00.829-00:00"}
+   {:id 10 :age 37 :event-time #inst "2015-09-13T03:45:00.829-00:00"}
+   {:id 3  :age 3  :event-time #inst "2015-09-13T03:05:00.829-00:00"}
+   {:id 11 :age 15 :event-time #inst "2015-09-13T03:03:00.829-00:00"}
+   {:id 12 :age 22 :event-time #inst "2015-09-13T03:56:00.829-00:00"}
+   {:id 13 :age 83 :event-time #inst "2015-09-13T03:59:00.829-00:00"}
+   {:id 14 :age 60 :event-time #inst "2015-09-13T03:32:00.829-00:00"}
+   {:id 15 :age 35 :event-time #inst "2015-09-13T03:16:00.829-00:00"}])
+
+(def expected-windows
+  [[Double/NEGATIVE_INFINITY 
+    Double/POSITIVE_INFINITY
+    #{{:id 1,
+       :age 21,
+       :event-time #inst "2015-09-13T03:00:00.829-00:00"}
+      {:id 2,
+       :age 12,
+       :event-time #inst "2015-09-13T03:04:00.829-00:00"}
+      {:id 3,
+       :age 3,
+       :event-time #inst "2015-09-13T03:05:00.829-00:00"}
+      {:id 4,
+       :age 64,
+       :event-time #inst "2015-09-13T03:06:00.829-00:00"}
+      {:id 5,
+       :age 53,
+       :event-time #inst "2015-09-13T03:07:00.829-00:00"}
+      {:id 6,
+       :age 52,
+       :event-time #inst "2015-09-13T03:08:00.829-00:00"}
+      {:id 7,
+       :age 24,
+       :event-time #inst "2015-09-13T03:09:00.829-00:00"}
+      {:id 8,
+       :age 35,
+       :event-time #inst "2015-09-13T03:15:00.829-00:00"}
+      {:id 9,
+       :age 49,
+       :event-time #inst "2015-09-13T03:25:00.829-00:00"}
+      {:id 10,
+       :age 37,
+       :event-time #inst "2015-09-13T03:45:00.829-00:00"}
+      {:id 11,
+       :age 15,
+       :event-time #inst "2015-09-13T03:03:00.829-00:00"}
+      {:id 12,
+       :age 22,
+       :event-time #inst "2015-09-13T03:56:00.829-00:00"}
+      {:id 13,
+       :age 83,
+       :event-time #inst "2015-09-13T03:59:00.829-00:00"}
+      {:id 14,
+       :age 60,
+       :event-time #inst "2015-09-13T03:32:00.829-00:00"}
+      {:id 15,
+       :age 35,
+       :event-time #inst "2015-09-13T03:16:00.829-00:00"}}]])
+
+(def test-state (atom []))
+
+(defn update-atom! [event window trigger {:keys [window-id upper-bound lower-bound]} state]
+  (swap! test-state conj [lower-bound upper-bound (set state)]))
+
+(def in-chan (atom nil))
+
+(def out-chan (atom nil))
+
+(defn inject-in-ch [event lifecycle]
+  {:core.async/chan @in-chan})
+
+(defn inject-out-ch [event lifecycle]
+  {:core.async/chan @out-chan})
+
+(def in-calls
+  {:lifecycle/before-task-start inject-in-ch})
+
+(def out-calls
+  {:lifecycle/before-task-start inject-out-ch})
+
+(defn base-job [batch-size] 
+  {:workflow [[:in :identity] [:identity :out]]
+   :catalog [{:onyx/name :in
+              :onyx/plugin :onyx.plugin.core-async/input
+              :onyx/type :input
+              :onyx/medium :core.async
+              :onyx/batch-size batch-size
+              :onyx/max-peers 1
+              :onyx/doc "Reads segments from a core.async channel"}
+
+             {:onyx/name :identity
+              :onyx/fn :clojure.core/identity
+              :onyx/type :function
+              :onyx/max-peers 1
+              :onyx/uniqueness-key :id
+              :onyx/batch-size batch-size}
+
+             {:onyx/name :out
+              :onyx/plugin :onyx.plugin.core-async/output
+              :onyx/type :output
+              :onyx/medium :core.async
+              :onyx/batch-size batch-size
+              :onyx/max-peers 1
+              :onyx/doc "Writes segments to a core.async channel"}]
+   :windows [{:window/id :collect-segments
+              :window/task :identity
+              :window/type :global
+              :window/aggregation :onyx.windowing.aggregation/conj
+              :window/window-key :event-time}]
+   :triggers [{:trigger/window-id :collect-segments
+               :trigger/refinement :accumulating
+               :trigger/on :segment
+               :trigger/fire-all-extents? true
+               :trigger/notifications [:task-complete]
+               :trigger/threshold [1 :elements]
+               :trigger/sync ::update-atom!}]
+   :lifecycles [{:lifecycle/task :in
+                 :lifecycle/calls ::in-calls}
+                {:lifecycle/task :in
+                 :lifecycle/calls :onyx.plugin.core-async/reader-calls}
+                {:lifecycle/task :out
+                 :lifecycle/calls ::out-calls}
+                {:lifecycle/task :out
+                 :lifecycle/calls :onyx.plugin.core-async/writer-calls}]
+   :task-scheduler :onyx.task-scheduler/balanced})
+
+(deftest conj-test
+  (let [id (java.util.UUID/randomUUID)
+        config (load-config)
+        env-config (assoc (:env-config config) :onyx/id id)
+        peer-config (assoc (:peer-config config) :onyx/id id)
+        batch-size 20]
+
+    (reset! in-chan (chan (inc (count input))))
+    (reset! out-chan (chan (sliding-buffer (inc (count input)))))
+    (reset! test-state [])
+
+    (with-test-env [test-env [3 env-config peer-config]]
+
+      (let [job (onyx.api/submit-job peer-config (base-job batch-size))]
+        (doseq [i input]
+          (>!! @in-chan i))
+        (>!! @in-chan :done)
+
+        (close! @in-chan)
+
+        (let [results (take-segments! @out-chan)]
+          (is (= (into #{} input) (into #{} (butlast results))))
+          (is (= :done (last results)))
+          (onyx.api/await-job-completion peer-config (:job-id job))
+          (is (= expected-windows @test-state)))))))
