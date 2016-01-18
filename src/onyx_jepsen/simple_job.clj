fb979eb1e97563a31c82cac02d110dea297344b0
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-01-18 20:11:26 +0800

    Drop restart-pred-fn and use :lifecycle/handle-exception

--- src/onyx_jepsen/simple_job.clj
+++ src/onyx_jepsen/simple_job.clj
@@ -1,6 +1,12 @@
 (ns onyx-jepsen.simple-job
   (:require [taoensso.timbre :refer [info error debug fatal]]))
 
+;; TODO, tighten up to test whether onyx-bookkeeper handles all its exceptions properly
+(defn handle-exception [event lifecycle lf-kw exception]
+  :restart)
+
+(def restart-calls
+  {:lifecycle/handle-exception handle-exception})
 
 (defn add-read-ledgers [job batch-size zk-addr ledgers-root-path password ledger-ids]
   (let [read-ledger-task-names (mapv (fn [id]
@@ -29,7 +35,6 @@
                          :bookkeeper/deserializer-fn :onyx.compression.nippy/zookeeper-decompress
                          :bookkeeper/password-bytes password 
                          :bookkeeper/no-recovery? true
-                         :onyx/restart-pred-fn :onyx-peers.functions.functions/restartable?
                          :onyx/max-peers 1
                          :onyx/batch-size batch-size
                          :onyx/doc "Reads a sequence from a BookKeeper ledger"})
@@ -37,10 +42,12 @@
                       ledger-ids))
         (update :lifecycles 
                 into 
-                (mapv (fn [task]
-                        {:lifecycle/task task
-                         :lifecycle/calls :onyx.plugin.bookkeeper/read-ledgers-calls})
-                      read-ledger-task-names)))))
+                (mapcat (fn [task]
+                          [{:lifecycle/task task
+                            :lifecycle/calls ::restart-calls}
+                           {:lifecycle/task task
+                            :lifecycle/calls :onyx.plugin.bookkeeper/read-ledgers-calls}])
+                        read-ledger-task-names)))))
 
 (defn build-job [job-num {:keys [batch-size] :as params} zk-addr ledgers-root-path ledger-ids]
   (let [password (.getBytes "INSECUREDEFAULTPASSWORD")
@@ -48,7 +55,6 @@
                         :onyx/plugin :onyx.plugin.bookkeeper/write-ledger
                         :onyx/type :output
                         :onyx/medium :bookkeeper
-                        :onyx/restart-pred-fn :onyx-peers.functions.functions/restartable?
                         :bookkeeper/serializer-fn :onyx.compression.nippy/zookeeper-compress
                         :bookkeeper/password-bytes password
                         :bookkeeper/ensemble-size 3
@@ -61,10 +67,13 @@
                         :onyx/fn :onyx-peers.functions.functions/add-job-num
                         :jepsen/job-num job-num
                         :onyx/params [:jepsen/job-num]
-                        :onyx/restart-pred-fn :onyx-peers.functions.functions/restartable?
                         :onyx/type :function
                         :onyx/batch-size batch-size}]
              :lifecycles [{:lifecycle/task :persist
+                           :lifecycle/calls ::restart-calls}
+                          {:lifecycle/task :identity-log
+                           :lifecycle/calls ::restart-calls}
+                          {:lifecycle/task :persist
                            :lifecycle/calls :onyx.plugin.bookkeeper/write-ledger-calls}]
              :workflow [[:identity-log :persist]]
              :task-scheduler :onyx.task-scheduler/balanced}] 
