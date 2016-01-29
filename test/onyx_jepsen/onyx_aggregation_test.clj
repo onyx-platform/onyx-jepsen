e30d7994a93d107cffe7a325d30a696be35f748c
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-01-29 17:01:05 +0800

    Delete scratch space code

--- test/onyx_jepsen/onyx_aggregation_test.clj
+++ test/onyx_jepsen/onyx_aggregation_test.clj
@@ -9,18 +9,6 @@
             [jepsen.core :as jc]
             [jepsen.control :as c]))
 
-(comment (def info (:information (:job-invariants (read-string (slurp "/Users/lucas/clojure/onyx-jepsen/store/onyx-aggregation-test/20160125T135056.000Z/results.edn")))))
-
-         (def read-values (set (:read-values info)))
-
-         (def not-triggered (set (:added-not-triggered info)))
-
-         (count read-values)
-         (count (identity not-triggered))
-         (keys info)
-         (count read-values)
-         (count not-triggered))
-
 (def version
   "What onyx version should we test?"
   "version-not-supplied")
