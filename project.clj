17deaf10d69e71069a7705ad85d376d5f1c29500
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-29 00:27:38 +0800

    Bump to final onyx versions

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.onyxplatform/onyx "0.9.0-SNAPSHOT" :exclusions [org.slf4j/slf4j-nop]]
+                 [org.onyxplatform/onyx "0.9.0" :exclusions [org.slf4j/slf4j-nop]]
                  [fipp "0.6.4"]
-                 [org.onyxplatform/onyx-metrics "0.9.0.0-beta1"]
-                 [org.onyxplatform/onyx-bookkeeper "0.9.0.0-beta1"]
+                 [org.onyxplatform/onyx-metrics "0.9.0.0"]
+                 [org.onyxplatform/onyx-bookkeeper "0.9.0.0"]
                  [jepsen "0.0.9"]]
   ;;; NOTE, don't swallow in BK
   :test-selectors {:jepsen :jepsen
